312733c1cb0b4b554e97829e94a7e6b4e9966680
Naseef A K <naseeefak@gmail.com>
2022-06-04 21:00:45 +0530

    Fn for transimitting integer values to UART
    wrote a function tx_sn() for transmitting integer vales upto 9999 to virtual terminal or any device using UART.

--- P03. DATA RECORDER/uart.h
+++ P03. DATA RECORDER/uart.h
@@ -1,6 +1,7 @@
 void ser_int();
 unsigned char tx(unsigned char);
 unsigned char rx();
+void tx_sn(unsigned int val); // fn for transmitting integer values to UART
 
 
 void ser_int()
@@ -21,4 +22,49 @@ unsigned char rx()
  while(!RCIF);
  RCIF=0;
  return RCREG;
+}
+
+void tx_sn (unsigned int val)
+{
+    int remainder, digit1, digit2, digit3, digit4, result, result1;
+    if (val<=9)
+    {
+       tx(val+0x30);
+    }
+    else if (val>=10 && val <100)
+    {
+        remainder = val % 10;
+        digit1 = remainder;
+        digit2 = val/10;
+        tx(digit2+0x30);
+        tx(digit1+0x30);
+    }
+    else if (val>=100 && val <1000)
+    {
+        remainder = val % 10;
+        result1 = val /10;
+        digit1 = remainder;
+        remainder = result1%10;
+        digit2 = remainder;
+        digit3 = result1/10;
+        tx(digit3+0x30);
+        tx(digit2+0x30);
+        tx(digit1+0x30);
+    }
+    else if (val>=1000 && val <9999)
+    {
+        remainder = val % 10;
+        result1 = val /10;
+        digit1 = remainder;
+        remainder = result1%10;
+        digit2 = remainder;
+        result = result1/10;
+        remainder = result%10;
+        digit3=remainder;
+        digit4 = result/10;
+        tx(digit4+0x30);
+        tx(digit3+0x30);
+        tx(digit2+0x30);
+        tx(digit1+0x30);
+    }
 }
