3e5bd844a0979d18fa3531e0ff82cd55571e56e5
Naseef A K <naseeefak@gmail.com>
2022-06-04 13:15:49 +0530

    UART Header file

---
+++ P03. DATA RECORDER/uart.h
@@ -0,0 +1,36 @@
+void ser_int();
+unsigned char tx(unsigned char);
+unsigned char rx();
+
+
+void ser_int()
+{
+ TXSTA= 0x20;
+ RCSTA= 0b10010000;
+ SPBRG=31;
+}
+ 
+unsigned char tx(unsigned char a)
+{
+ TXREG=a;
+ while(!TXIF);
+ TXIF = 0;
+}
+unsigned char rx()
+{
+ while(!RCIF);
+ RCIF=0;
+ return RCREG;
+}
+
+/*
+ char b; 
+TRISD=0x00;
+TRISB=0X00;
+LCD_init();
+ser_int(); 
+tx('a');
+b = tx('a');
+LCD_data (b);
+__delay_ms(100);
+ */
