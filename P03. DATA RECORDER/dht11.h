e3ce6acaa5158f5cf3578c5d9da65c8a3bdd7f16
Naseef A K <naseeefak@gmail.com>
2022-06-05 11:15:11 +0530

    Created Header file and Fns for DHT11
    Developed functions for initialization, finding response and reading temperature and humidity data from DHT11 sensor and moved to another header file named "dht11.h"

---
+++ P03. DATA RECORDER/dht11.h
@@ -0,0 +1,57 @@
+#define _XTAL_FREQ 20000000
+
+#define DHT11_Data_Pin_Direction  TRISDbits.TRISD0
+#define DHT11_Data_Pin   PORTDbits.RD0
+
+// Variables for DHT11
+unsigned char Humidity, Temp, RH_byte_1, RH_byte_2, Temp_byte_1, Temp_byte_2; 
+unsigned char check_bit, Summation;
+
+void dht11_init();
+void find_response();
+char read_dht11();
+
+void dht11_init()
+{
+    DHT11_Data_Pin_Direction= 0; //Configure RD0 as output
+    DHT11_Data_Pin = 0; //RD0 sends 0 to the sensor
+    __delay_ms(18);
+    DHT11_Data_Pin = 1; //RD0 sends 1 to the sensor
+    __delay_us(30);
+    DHT11_Data_Pin_Direction = 1; //Configure RD0 as input
+ }
+
+ void find_response()
+{
+    check_bit = 0;
+    __delay_us(40);
+    if (DHT11_Data_Pin == 0)
+    {
+        __delay_us(80);
+        if (DHT11_Data_Pin == 1)
+        {
+            check_bit = 1;
+        }          
+        __delay_us(50);;
+    }
+ }
+ 
+char read_dht11()
+{
+    char data, for_count;
+    for(for_count = 0; for_count < 8; for_count++)
+    {
+        while(!DHT11_Data_Pin);
+        __delay_us(30);
+        if(DHT11_Data_Pin == 0)
+        {
+            data&= ~(1<<(7 - for_count)); //Clear bit (7-b)
+        }
+        else
+        {
+            data|= (1 << (7 - for_count)); //Set bit (7-b)
+            while(DHT11_Data_Pin);
+        }
+    }
+    return data;
+}
