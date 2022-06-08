41e541f58d4ae0689d23aa3464029242abcd058d
Naseef A K <naseeefak@gmail.com>
2022-06-08 21:40:21 +0530

    Header file created for BMP280
    header file and functions are successfully created for bmp280 and verified

---
+++ P03. DATA RECORDER/bmp280.h
@@ -0,0 +1,87 @@
+#define write_cmd 0xEE
+#define read_cmd 0xEF
+
+void bmp280_init();
+void bmp280_start();
+void bmp280_stop();
+void bmp280_restart();
+void bmp280_ack();
+void bmp280_nak();
+void bmp280_waitmssp();
+void bmp280_send(unsigned char dat);
+void bmp280_send_byte(unsigned char addr,unsigned char count);
+unsigned char bmp280_read();
+unsigned char bmp280_read_byte(unsigned char addr);
+void bmp280_init()
+{
+    TRISC3=TRISC4=1;
+    SSPCON=0x28;                    //SSP Module as Master
+    SSPADD=((11059200/4)/100)-1;    //Setting Clock Speed, My PCLK = 11.0592MHz
+}
+void bmp280_start()
+{
+    SEN=1;
+    bmp280_waitmssp();
+}
+void bmp280_stop()
+{
+    PEN=1;
+    bmp280_waitmssp();
+}
+void bmp280_restart()
+{
+    RSEN=1;
+    bmp280_waitmssp();
+}
+void bmp280_ack()
+{
+    ACKDT=0;
+    ACKEN=1;
+    bmp280_waitmssp();
+}
+void bmp280_nak()
+{
+    ACKDT=1;
+    ACKEN=1;
+    bmp280_waitmssp();
+}
+void bmp280_waitmssp()
+{
+    while(!SSPIF);
+    SSPIF=0;
+}
+void bmp280_send(unsigned char dat)
+{
+L1: SSPBUF=dat;
+    bmp280_waitmssp();
+    while(ACKSTAT){bmp280_restart;goto L1;}
+}
+void bmp280_send_byte(unsigned char addr,unsigned char count)
+{
+    bmp280_start();
+    bmp280_send(write_cmd);
+    bmp280_send(addr);
+    bmp280_send(count);
+    bmp280_stop();
+}
+unsigned char bmp280_read()
+{
+    RCEN=1;
+    bmp280_waitmssp();
+    return SSPBUF;
+}   
+unsigned char bmp280_read_byte(unsigned char addr)
+{
+    unsigned char rec;
+L:  bmp280_restart();
+    SSPBUF=write_cmd;
+    bmp280_waitmssp();
+    while(ACKSTAT){goto L;}
+    bmp280_send(addr);
+    bmp280_restart();
+    bmp280_send(read_cmd);
+    rec=bmp280_read();
+    bmp280_nak();
+    bmp280_stop();
+    return rec;
+}
