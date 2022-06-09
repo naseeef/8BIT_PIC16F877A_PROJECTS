3a9c9968ea5e05256506ce324c81fb4a4891728a
Naseef A K <naseeefak@gmail.com>
2022-06-09 17:29:23 +0530

    new header file for i2c fns, renamed as  bmp280_......()
    void bmp280_init();
    void bmp280_start();
    void bmp280_stop();
    void bmp280_restart();
    void bmp280_ack();
    void bmp280_nak();
    void bmp280_waitmssp();
    void bmp280_send(unsigned char dat);
    void bmp280_send_byte(unsigned char addr,unsigned char count);
    unsigned char bmp280_read();
    unsigned char bmp280_read_byte(unsigned char addr);
    void bmp280_init()

--- P03. DATA RECORDER/P03.C.BMP280_INTERFACING.X/p03a_bmp280.h
+++ P03. DATA RECORDER/P03.C.BMP280_INTERFACING.X/p03a_bmp280.h
@@ -1,91 +1,87 @@
 #define write_cmd 0xEE
 #define read_cmd 0xEF
 
-void i2c_init();
-void i2c_start();
-void i2c_stop();
-void i2c_restart();
-void i2c_ack();
-void i2c_nak();
-void waitmssp();
-void i2c_send(unsigned char dat);
-void i2c_send_byte(unsigned char addr,unsigned char *count);
-unsigned char i2c_read();
-unsigned char i2c_read_byte(unsigned char addr);
-void i2c_init()
+void bmp280_init();
+void bmp280_start();
+void bmp280_stop();
+void bmp280_restart();
+void bmp280_ack();
+void bmp280_nak();
+void bmp280_waitmssp();
+void bmp280_send(unsigned char dat);
+void bmp280_send_byte(unsigned char addr,unsigned char count);
+unsigned char bmp280_read();
+unsigned char bmp280_read_byte(unsigned char addr);
+void bmp280_init()
 {
     TRISC3=TRISC4=1;
     SSPCON=0x28;                    //SSP Module as Master
     SSPADD=((11059200/4)/100)-1;    //Setting Clock Speed, My PCLK = 11.0592MHz
 }
-void i2c_start()
+void bmp280_start()
 {
     SEN=1;
-    waitmssp();
+    bmp280_waitmssp();
 }
-void i2c_stop()
+void bmp280_stop()
 {
     PEN=1;
-    waitmssp();
+    bmp280_waitmssp();
 }
-void i2c_restart()
+void bmp280_restart()
 {
     RSEN=1;
-    waitmssp();
+    bmp280_waitmssp();
 }
-void i2c_ack()
+void bmp280_ack()
 {
     ACKDT=0;
     ACKEN=1;
-    waitmssp();
+    bmp280_waitmssp();
 }
-void i2c_nak()
+void bmp280_nak()
 {
     ACKDT=1;
     ACKEN=1;
-    waitmssp();
+    bmp280_waitmssp();
 }
-void waitmssp()
+void bmp280_waitmssp()
 {
     while(!SSPIF);
     SSPIF=0;
 }
-void i2c_send(unsigned char dat)
+void bmp280_send(unsigned char dat)
 {
 L1: SSPBUF=dat;
-    waitmssp();
-    while(ACKSTAT){i2c_restart;goto L1;}
+    bmp280_waitmssp();
+    while(ACKSTAT){bmp280_restart;goto L1;}
 }
-void i2c_send_byte(unsigned char addr,unsigned char *count)
+void bmp280_send_byte(unsigned char addr,unsigned char count)
 {
-    i2c_start();
-    i2c_send(write_cmd);
-    i2c_send(addr>>8);
-    i2c_send(addr);
-    while(*count) {
-        i2c_send(*count++);
-    }
-    i2c_stop();
+    bmp280_start();
+    bmp280_send(write_cmd);
+    bmp280_send(addr);
+    bmp280_send(count);
+    bmp280_stop();
 }
-unsigned char i2c_read()
+unsigned char bmp280_read()
 {
     RCEN=1;
-    waitmssp();
+    bmp280_waitmssp();
     return SSPBUF;
 }   
-unsigned char i2c_read_byte(unsigned char addr)
+unsigned char bmp280_read_byte(unsigned char addr)
 {
     unsigned char rec;
-L:  i2c_restart();
+L:  bmp280_restart();
     SSPBUF=write_cmd;
-    waitmssp();
+    bmp280_waitmssp();
     while(ACKSTAT){goto L;}
-    i2c_send(addr>>8);
-    i2c_send(addr);
-    i2c_restart();
-    i2c_send(read_cmd);
-    rec=i2c_read();
-    i2c_nak();
-    i2c_stop();
+    bmp280_send(addr);
+    bmp280_restart();
+    bmp280_send(read_cmd);
+    rec=bmp280_read();
+    bmp280_nak();
+    bmp280_stop();
     return rec;
 }
